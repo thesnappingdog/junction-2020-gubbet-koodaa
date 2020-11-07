92a017bd950bd44c5f0ac98f227cc301da793eba
jsatka <jmksatka@gmail.com>
2020-11-07 14:05:35 +0200

    WS server: Check type of incoming data

--- listener-backend/server.py
+++ listener-backend/server.py
@@ -1,13 +1,18 @@
 import asyncio
 import websockets
 
-counter = 0
+text_frame_counter = 0
 
 async def handler(websocket, path):
-    async for message in websocket:
-        print(">> " + message)
-        await websocket.send(message)
-        print("Sent reply #{}: {}", counter, message)
+    async for frame in websocket:
+        if type(frame) is bytes:
+            print("<< ({} bytes of binary data)", len(frame))
+            # ToDo: Parse player ID
+            # ToDo: Send audio to Julle's amazing AI backend
+        elif type(frame) is str:
+            print("<< " + frame)
+            await websocket.send(frame)
+            print(">> {}", frame)
 
 asyncio.get_event_loop().run_until_complete(websockets.serve(handler, 'localhost', 3012))
 asyncio.get_event_loop().run_forever()
